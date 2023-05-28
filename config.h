248212cc20ca9c798308873f5dbf39cd8c0e0c0e
Pablo Jiménez Pascual <pablo@jimpas.me>
2023-05-28 17:55:04 +0200

    Use Go font

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
+	"Go:size=12:antialias=true:autohint=true"
 	"monospace:size=12",
 	"NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"
 };
